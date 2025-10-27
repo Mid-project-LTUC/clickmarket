48fd7d47f033e20889daf90daacfce66faa80158
mohammadalmoqdad9 <almoqdad585@gmail.com>
2025-10-27 22:41:55 +0300

    forget this update

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,21 +1,57 @@
 
-import { Link } from "react-router-dom";
-import SearchBar from "./Searchbar";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
+
+
+
+
+
+import { useEffect } from "react";
+import "./header.css";
 
 export default function Header() {
+  useEffect(() => {
+    const header = document.getElementById("main-header");
+
+    const handleScroll = () => {
+      if (window.scrollY > 50) header.classList.add("scrolled");
+      else header.classList.remove("scrolled");
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <header>
-      <div className="header-top">
-        <h1>Click Market</h1>
-        <SearchBar/>
+    <header id="main-header">
+      <div className="navbar-container">
+        <div className="logo">ClickMarket</div>
+
+        <nav className="nav-links">
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Products</a>
+          <a href="#">Offers</a>
+          <a href="#">Contact</a>
+        </nav>
+
+        <div className="search-bar">
+          <input type="text" placeholder="Search..." />
+          <button>
+            <i className="fa fa-search"></i>
+          </button>
+        </div>
+
         <div className="icons">
-          <Link to="/cart">Cart</Link>
-          <FontAwesomeIcon icon="fa-solid fa-cart-shopping" />
-          <Link to="/wishlist">WishList</Link> 
-          <FontAwesomeIcon icon="fa-solid fa-heart" />        
-          <Link to="/user">User</Link>
-          <FontAwesomeIcon icon="fa-solid fa-user" />
+          <button className="icon-btn">
+            <i className="fa fa-heart"></i>
+          </button>
+          <button className="icon-btn">
+            <i className="fa fa-shopping-cart"></i>
+          </button>
+          <button className="icon-btn">
+            <i className="fa fa-user"></i>
+          </button>
         </div>
       </div>
     </header>
