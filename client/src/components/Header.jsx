7eb3158fad8b90f3f8399542cbb163b40c7c52bc
mohammadalmoqdad9 <almoqdad585@gmail.com>
2025-10-28 14:50:56 +0300

    add images

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
-
-
-
-
-
-
-
 
 import { useEffect } from "react";
 import "./header.css";
 
-=======
+
 import { Link } from "react-router-dom";
 import SearchBar from "./Searchbar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -21,7 +13,7 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import "./Header.css";
 import "./Header.css";
->>>>>>> c426fc4ab79fb6a497348947dc9e7ff577a45cb8
+
 export default function Header() {
   useEffect(() => {
     const header = document.getElementById("main-header");
@@ -36,37 +28,6 @@ export default function Header() {
   }, []);
 
   return (
-<<<<<<< HEAD
-    <header id="main-header">
-      <div className="navbar-container">
-        <div className="logo">ClickMarket</div>
-
-        <nav className="nav-links">
-          <a href="#">Home</a>
-          <a href="#">About</a>
-          <a href="#">Products</a>
-          <a href="#">Offers</a>
-          <a href="#">Contact</a>
-        </nav>
-
-        <div className="search-bar">
-          <input type="text" placeholder="Search..." />
-          <button>
-            <i className="fa fa-search"></i>
-          </button>
-        </div>
-
-        <div className="icons">
-          <button className="icon-btn">
-            <i className="fa fa-heart"></i>
-          </button>
-          <button className="icon-btn">
-            <i className="fa fa-shopping-cart"></i>
-          </button>
-          <button className="icon-btn">
-            <i className="fa fa-user"></i>
-          </button>
-=======
     <header>
       <div className="header-top">
         <h1>Click Market</h1>
@@ -81,7 +42,6 @@ export default function Header() {
           <Link to="/user" className="icon">
             <FontAwesomeIcon icon={faUser} />
           </Link>
->>>>>>> c426fc4ab79fb6a497348947dc9e7ff577a45cb8
         </div>
       </div>
     </header>
